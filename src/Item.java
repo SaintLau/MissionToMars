8bb75b5329cb61d24307941445a84d2ecd793f3c
laura.santos <laura-santos@future-healthcare.eu>
2022-05-02 17:46:56 +0100

    add Item class

---
+++ src/Item.java
@@ -0,0 +1,12 @@
+//  Class that will represent the item that will be carried by the rockets
+
+
+public class Item {
+    String name; // name of the item
+    int weight; //weight of the item
+
+    //Constructor
+    Item() {
+    
+    }
+}
