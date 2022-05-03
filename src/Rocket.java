93301a7cbb788b21c5f722b77e79a12e6108fdd3
laura.santos <laura-santos@future-healthcare.eu>
2022-05-03 15:31:13 +0100

    Rocket class with logic

--- src/Rocket.java
+++ src/Rocket.java
@@ -20,7 +20,33 @@ public class Rocket implements SpaceShip {
 
     //Constructor
     Rocket() {
-        
+        //Start, still on land so all values are 0
+        currentWeight = 0;
+        cargoCarried = 0;
+        chanceLaunchExplosion = 0.0; //the chance will be a % between (cargo carried / cargo limit)
+        chanceLandingCrash = 0.0; //the chance will be a % between (cargo carried / cargo limit)
+        cargoLimit = 0; //measure in kilograms
+        rocketStatus = "Waiting for launch";
+        random = new Random().nextDouble(); //generates random num between 0 and 0.99999 but never reach 1
+    }
+
+    //launched and land methods
+    public boolean launch() {
+        return true;
+    }
+    public boolean land() {
+        return true;
+    }
+
+    //Carry the Item method -> true it can carry item or false it cannot carry item
+    public boolean canCarry(Item cargo) {
+        return (this.currentWeight + cargo.weight) <= rocketMaxWeight;
+    }
+
+    //Method that updates the weight of rocket
+    public void carry(Item cargo) {
+        this.currentWeight = this.currentWeight + cargo.weight;
+        this.cargoCarried = this.currentWeight - this.rocketWeight;
     }
 
 }
